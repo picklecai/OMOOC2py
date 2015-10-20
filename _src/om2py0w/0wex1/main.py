b8fbfff515f6b304989b254ce2027eaa3d9c7f0b
picklecai <52421240@qq.com>
2015-10-20 16:02:52 +0800

    Update main.py

--- _src/om2py0w/0wex1/main.py
+++ _src/om2py0w/0wex1/main.py
@@ -1,12 +1,20 @@
-# _*_ coding:utf-8_*_
+# _*_ coding:utf-8 _*_
 
-txt = open("tempfile.txt")
-notelist = txt.readlines()
-txt.close()
-for i in notelist:
-	print(i)
+from os.path import exists
+
+if exists("tempfile.txt"):
+	print '''
+记事本现在的内容是：
+-------------------------------------------'''
+	txt = open("tempfile.txt")
+	notelist = txt.readlines()
+	txt.close()
+	for i in notelist:
+		print(i)
+	print "-------------------------------------------"
+
 txt = open("tempfile.txt", 'a')
-newline = raw_input(">")
+newline = raw_input("请给记事本输入新内容： ")
 txt.write(newline)
 txt.write("\n")
 txt.close()
