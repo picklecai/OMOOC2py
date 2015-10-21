fe2182f1c8d41dbacb136049df24bf0e892d0ea1
picklecai <52421240@qq.com>
2015-10-21 15:01:08 +0800

    Update main.py

--- _src/om2py0w/0wex1/main.py
+++ _src/om2py0w/0wex1/main.py
@@ -2,6 +2,7 @@
 
 from os.path import exists
 
+# 打印之前所有的内容
 if exists("tempfile.txt"):
 	print '''
 记事本现在的内容是：
@@ -13,8 +14,16 @@ if exists("tempfile.txt"):
 		print(i)
 	print "-------------------------------------------"
 
+# 输入新的内容
 txt = open("tempfile.txt", 'a')
 newline = raw_input("请给记事本输入新内容： ")
 txt.write(newline)
 txt.write("\n")
 txt.close()
+
+# 新内容另存为文件
+filename = raw_input("新内容的文件名是：")
+output = open(filename + ".txt", 'w')
+output.write(newline)
+output = open(filename + ".txt")
+output.close
