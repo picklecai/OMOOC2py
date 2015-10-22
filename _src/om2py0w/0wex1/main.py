3fb263923294cecd7a196d9665c783b9e40c7abb
picklecai <52421240@qq.com>
2015-10-22 16:21:44 +0800

    Update main.py

--- _src/om2py0w/0wex1/main.py
+++ _src/om2py0w/0wex1/main.py
@@ -1,9 +1,31 @@
 # _*_ coding:utf-8 _*_
 
 from os.path import exists
+from sys import exit
 import time
 
 def main():
+	print '''
+请输入你的选择：
+1 打印之前的历史记录
+2 输入新的内容
+3 退出程序 '''
+	prompt = '>'
+	choose = raw_input(prompt)
+
+	if choose =="1" :
+		printhistory()
+		main()
+	elif choose =="2" :
+		writenew()
+		main()
+	elif choose =="3" :
+		exit(0)
+	else:
+		print "对不起，我不知道你想要的是什么。"
+		main()
+
+def printhistory():
 	# 打印之前所有的内容
 	if exists("tempfile.txt"):   #如果tempfile不存在，说明程序是第一次运行，没有历史内容可打印。
 		print '''
@@ -16,6 +38,7 @@ def main():
 			print(i)
 		print "-------------------------------------------"
 
+def writenew():
 	# 输入新的内容
 	txt = open("tempfile.txt", 'a')
 	newline = raw_input("今日记录，请输入： ")
@@ -24,14 +47,27 @@ def main():
 	txt.write("\n"+"\n")
 	txt.close()
 
-	# 新内容另存为文件
-	filename = raw_input("存储今日记录，请输入文件名：")
-	output = open(filename + ".txt", 'w') #规定好扩展名，以免出现用户保存的文件打不开的情况。
-	output.write(filename + ".txt"+"\n") #在文件中写入文件名。
-	output.write(time.strftime("%d/%m/%Y %H:%M:%S"+"\n")) #在文件中写入当前时间。
-	output.write(newline)
-	output = open(filename + ".txt")
-	output.close
+	print '''
+你是否想把新输入内容存为独立文件？ 
+1 好的，存为独立文件。 
+2 不想，只是随便写写。	'''
+	prompt = '>'
+	choose = raw_input(prompt)
+
+	if choose =="1" :
+		# 新内容另存为文件
+		filename = raw_input("存储今日记录，请输入文件名：")
+		output = open(filename + ".txt", 'w') #规定好扩展名，以免出现用户保存的文件打不开的情况。
+		output.write(filename + ".txt"+"\n") #在文件中写入文件名。
+		output.write(time.strftime("%d/%m/%Y %H:%M:%S"+"\n")) #在文件中写入当前时间。
+		output.write(newline)
+		output = open(filename + ".txt")
+		output.close
+		main()
+	elif choose =="2" :
+		main()
+	elif choose =="3" :
+		exit(0)
 
 if __name__ == '__main__':
 	main()
