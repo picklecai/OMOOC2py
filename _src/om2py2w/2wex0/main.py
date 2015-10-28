f2fc369c48746136509a29f73d5765b22c046515
picklecai <52421240@qq.com>
2015-10-28 09:43:01 +0800

    Update main.py

--- _src/om2py2w/2wex0/main.py
+++ _src/om2py2w/2wex0/main.py
@@ -3,18 +3,18 @@
 from os.path import exists
 from sys import exit
 import time
-from Tkinter import *
+import Tkinter
 
 def main():
     global win
-    win = Tk()
+    win = Tkinter.Tk()
     win.title = ('Picklecai notebook' )
     win.geography =('640x480')
 
     # 放置三个按钮供选择
-    b1 = Button(win, text = "打印历史记录", command = printhistory)
-    b2 = Button(win, text = "输入新的内容", command = writenew)
-    b3 = Button(win, text = "退出程序", command = quit())
+    b1 = Tkinter.Button(win, text = "打印历史记录", command = printhistory)
+    b2 = Tkinter.Button(win, text = "输入新的内容", command = writenew)
+    b3 = Tkinter.Button(win, text = "退出程序", command = quit())
 
     b1.grid(row = 0, column = 0)
     b2.grid(row = 0, column = 1)
@@ -29,8 +29,8 @@ def printhistory():
 历史记录：
 -------------------------------------------'''
     historynone = "对不起，程序第一次运行，尚无历史记录可打印。你可以开始输入新内容。"
-    l1 = Label(win, text = historyfirst)
-    l2 = Label(win, text = historynone)
+    l1 = Tkinter.Label(win, text = historyfirst)
+    l2 = Tkinter.Label(win, text = historynone)
 
     if exists("tempfile.txt"):   #如果tempfile不存在，说明程序是第一次运行，没有历史内容可打印。
         txt = open("tempfile.txt")
@@ -46,9 +46,9 @@ def printhistory():
 def writenew():
     # 输入新的内容
     newlinefirst = "今日记录，请输入： "
-    l3 = Label(win, text = newlinefirst)
+    l3 = Tkinter.Label(win, text = newlinefirst)
     l3.grid(row =2, column = 0)
-    newline = Entry(win)
+    newline = Tkinter.Entry(win)
     newline.grid(row = 2, column = 1)    
 
     txt = open("tempfile.txt", 'a')
@@ -57,7 +57,7 @@ def writenew():
     txt.write("\n"+"\n")
     txt.close()
 
-    b4 = Button(win, text = "保存新内容", command = savenew)
+    b4 = Tkinter.Button(win, text = "保存新内容", command = savenew)
     def savenew():
             filename = Entry(win)
             output = open(filename + ".txt", 'w') #规定好扩展名，以免出现用户保存的文件打不开的情况。
