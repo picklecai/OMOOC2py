b90a5118fe92b0514d805ac36a0ae1b9dde7113b
picklecai <52421240@qq.com>
2015-12-06 16:23:39 +0800

    update main.py

--- _src/om2py7w/7wex0/main.py
+++ _src/om2py7w/7wex0/main.py
@@ -15,6 +15,7 @@ import sqlite3
 ASSETS = "/assets/"
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
+
 class MyWSGIRefServer(ServerAdapter):
     server = None
 
@@ -28,60 +29,69 @@ class MyWSGIRefServer(ServerAdapter):
         self.server.serve_forever()
 
     def stop(self):
-        #sys.stderr.close()
-        import threading 
-        threading.Thread(target=self.server.shutdown).start() 
-        #self.server.shutdown()
-        self.server.server_close() 
+        # sys.stderr.close()
+        import threading
+        threading.Thread(target=self.server.shutdown).start()
+        # self.server.shutdown()
+        self.server.server_close()
         print "# QWEBAPPEND"
 
+
 def __exit():
-	global server
-	server.stop()
+    global server
+    server.stop()
+
 
 def __ping():
-	return "OK"
+    return "OK"
+
 
 def server_static(filepath):
     return static_file(filepath, root=ROOT+'/assets')
 
+
 def home():
+    inputnewline()
     return template(ROOT+'/index.html')
+
 
 def save(newline):
     conn = sqlite3.connect('/storage/emulated/0/com.hipipal.qpyplus/project/notebookapp/noterecord.db')
     cursor = conn.cursor()
     cursor.execute('create table record (id int(4) primary key, time text, record varchar)')
     id = 1
-    newline=request.Get['newline']
+    newline = request.get['newline']
     cursor.execute('insert into record (id, time, record) values (\'id\', time.strftime("%d/%m/%Y %H:%M:%S"), \'newline\')')
     cursor.close()
     conn.commit()
     conn.close()
 
-def printhistory():
-    if exists("noterecord.db"): 
-        conn = sqlite3.connect('noterecord.db')
-        cursor = conn.cursor()
-        cursor.execute('select * from record')
-        return cursor.fetchall()
 
 @post('/index.html')
+def printhistory():
+    conn = sqlite3.connect('noterecord.db')
+    cursor = conn.cursor()
+    cursor.execute('select * from record')
+    return cursor.fetchall()
+
+
+@get('/index.html')
 def inputnewline():
-    newline = request.GET('newline')
-    if newline:
-        save(newline)
-        printhistory()
-        return newline
+    newline = request.get('newline')
+    return '''
+    <button type="button" name="save" class="am-btn am-btn-primary am-round" onclick="save(newline"><i class="am-icon-search"></i> save</button>
+    <label cols="40" rows="8" name="historylabel" style="">printhistory()</label>
+    '''
+
 
 app = Bottle()
 app.route('/', method='GET')(home)
-app.route('/__exit', method=['GET','HEAD'])(__exit)
-app.route('/__ping', method=['GET','HEAD'])(__ping)
+app.route('/__exit', method=['GET', 'HEAD'])(__exit)
+app.route('/__ping', method=['GET', 'HEAD'])(__ping)
 app.route('/assets/<filepath:path>', method='GET')(server_static)
 
 try:
     server = MyWSGIRefServer(host="localhost", port="8800")
-    app.run(server=server,reloader=False)
-except Exception,ex:
+    app.run(server=server, reloader=False)
+except Exception, ex:
     print "Exception: %s" % repr(ex)
