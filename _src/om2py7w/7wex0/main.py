817328963c4e7a7e0091ca6ae4e617b0720f26fd
picklecai <52421240@qq.com>
2015-12-07 16:44:58 +0800

    update

--- _src/om2py7w/7wex0/main.py
+++ _src/om2py7w/7wex0/main.py
@@ -43,19 +43,17 @@ def __exit():
 def __ping():
     return "OK"
 
-def server_static(filepath):
-    return static_file(filepath, root=ROOT+'/assets')
-
 def inputnewline(data):
-    conn = sqlite3.connect(ROOT+'noterecord.db')
+    conn = sqlite3.connect(ROOT+'/noterecord.db')
     cursor = conn.cursor()
+    cursor.execute('create table if not exists record (time text, record varchar)')
     cursor.execute('insert into record (time, record) values (?,?)', data)
     cursor.close()
     conn.commit()
     conn.close()
 
 def printhistory():
-    conn = sqlite3.connect(ROOT+'noterecord.db')
+    conn = sqlite3.connect(ROOT+'/noterecord.db')
     cursor = conn.cursor()
     cursor.execute('select * from record')
     return cursor.fetchall()
@@ -67,16 +65,15 @@ app = Bottle()
 app.route('/', method='GET')(home)
 app.route('/__exit', method=['GET', 'HEAD'])(__exit)
 app.route('/__ping', method=['GET', 'HEAD'])(__ping)
-app.route('/assets/<filepath:path>', method='GET')(server_static)
 
 @app.route('/index.html', method='POST')
 def save():
-    newline = request.get('newline')
+    newline = request.forms.get('newline')
     nowtime = time.time()
-    return '''
-    <button type="button" name="save" class="am-btn am-btn-primary am-round" onclick="save(newline)"><i class="am-icon-search"></i> save</button>
-    <label cols="40" rows="8" name="historylabel" style="">printhistory()</label>
-    '''
+    data = nowtime, newline
+    inputnewline(data)
+    records = printhistory() 
+    return template(ROOT+'/index.html', historylabel=records)
 
 try:
     server = MyWSGIRefServer(host="localhost", port="8800")
